229dc08c6d0662495c5f2deda9e7db02584b1b7e
Kaio C. de Oliveira <hello@kaio.dev>
2022-10-29 13:59:44 -0300

    adiciona app text styles

---
+++ lib/common/constants/app_text_styles.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/painting.dart';
+
+class AppTextStyles {
+  AppTextStyles._();
+
+  static const TextStyle bigText = TextStyle(
+    fontSize: 50.0,
+    fontWeight: FontWeight.w700,
+  );
+}
