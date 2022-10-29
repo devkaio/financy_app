b64c9ef79793865b93e5886d665c975a9e1b9b38
Kaio C. de Oliveira <hello@kaio.dev>
2022-10-29 13:12:04 -0300

    atualiza app

---
+++ lib/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Home'),
+        ),
+      ),
+    );
+  }
+}
