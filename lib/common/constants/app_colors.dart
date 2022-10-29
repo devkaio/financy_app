e209dfa71a3eaf883b2d90a478514bd1e9b4f848
Kaio C. de Oliveira <hello@kaio.dev>
2022-10-29 13:59:37 -0300

    adiciona app colors

---
+++ lib/common/constants/app_colors.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+class AppColors {
+  AppColors._();
+
+  static const Color greelightOne = Color(0xFF63B5AF);
+  static const Color greelightTwo = Color(0xFF438883);
+  static const List<Color> greenGradient = [
+    Color(0xFF63B5AF),
+    Color(0xFF438883),
+  ];
+  static const Color white = Color(0xFFFFFFFF);
+}
