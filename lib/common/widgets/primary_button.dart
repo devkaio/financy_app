7c6e02436f7f1a89658cd70b9199acc244d6bc08
Kaio C. de Oliveira <hello@kaio.dev>
2022-10-30 11:09:05 -0300

    refatora primary button

--- lib/common/widgets/primary_button.dart
+++ lib/common/widgets/primary_button.dart
@@ -13,35 +13,31 @@ class PrimaryButton extends StatelessWidget {
     required this.text,
   }) : super(key: key);
 
+  final BorderRadius _borderRadius =
+      const BorderRadius.all(Radius.circular(24.0));
+
   @override
   Widget build(BuildContext context) {
-    return Material(
-      borderRadius: const BorderRadius.all(Radius.circular(38.0)),
-      child: Ink(
-        decoration: BoxDecoration(
-          borderRadius: const BorderRadius.all(Radius.circular(38.0)),
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: onPressed != null
-                ? AppColors.greenGradient
-                : AppColors.greyGradient,
-          ),
+    return Ink(
+      height: 48.0,
+      decoration: BoxDecoration(
+        borderRadius: _borderRadius,
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: onPressed != null
+              ? AppColors.greenGradient
+              : AppColors.greyGradient,
         ),
-        child: InkWell(
-          borderRadius: const BorderRadius.all(Radius.circular(38.0)),
-          onTap: onPressed,
-          child: Container(
-            decoration: const BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(38.0)),
-            ),
-            alignment: Alignment.center,
-            height: 64.0,
-            child: Text(
-              text,
-              style: AppTextStyles.mediumText18.copyWith(
-                color: AppColors.white,
-              ),
+      ),
+      child: InkWell(
+        borderRadius: _borderRadius,
+        onTap: onPressed,
+        child: Align(
+          child: Text(
+            text,
+            style: AppTextStyles.mediumText18.copyWith(
+              color: AppColors.white,
             ),
           ),
         ),
