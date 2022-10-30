cc591c433af3f799d9a8e8dbee8d5d62be1edc15
Kaio C. de Oliveira <hello@kaio.dev>
2022-10-29 22:49:29 -0300

    atualiza app

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,4 @@
-import 'package:financy_app/features/splash/splash_page.dart';
+import 'package:financy_app/features/onboarding/onboarding_page.dart';
 import 'package:flutter/material.dart';
 
 class App extends StatelessWidget {
@@ -7,7 +7,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: SplashPage(),
+      home: OnboardingPage(),
     );
   }
 }
