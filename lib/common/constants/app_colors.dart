5b2731f136faae49ba8e216c9f18524720b8d4d5
Kaio C. de Oliveira <hello@kaio.dev>
2022-10-29 23:47:05 -0300

    adiciona cores

--- lib/common/constants/app_colors.dart
+++ lib/common/constants/app_colors.dart
@@ -9,6 +9,10 @@ class AppColors {
     Color(0xFF63B5AF),
     Color(0xFF438883),
   ];
+  static const List<Color> greyGradient = [
+    Color(0xFFB5B5B5),
+    Color(0xFF7F7F7F),
+  ];
   static const Color white = Color(0xFFFFFFFF);
   static const Color iceWhite = Color(0xFFEEF8F7);
   static const Color grey = Color(0xFF444444);
