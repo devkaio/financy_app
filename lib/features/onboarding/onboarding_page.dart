60885b403e4081da54a57a6b8d243c25e2d40bcd
Kaio C. de Oliveira <hello@kaio.dev>
2022-10-30 14:18:46 -0300

    refatora onboarding

--- lib/features/onboarding/onboarding_page.dart
+++ lib/features/onboarding/onboarding_page.dart
@@ -1,9 +1,12 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 
+import 'dart:developer';
+
 import 'package:financy_app/common/constants/app_colors.dart';
 import 'package:financy_app/common/constants/app_text_styles.dart';
 import 'package:flutter/material.dart';
 
+import '../../common/widgets/multi_text_button.dart';
 import '../../common/widgets/primary_button.dart';
 
 class OnboardingPage extends StatelessWidget {
@@ -36,20 +39,33 @@ class OnboardingPage extends StatelessWidget {
               ),
             ),
             Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 32.0,
-                vertical: 16.0,
+              padding: const EdgeInsets.only(
+                left: 32.0,
+                right: 32.0,
+                top: 16.0,
+                bottom: 4.0,
               ),
               child: PrimaryButton(
                 text: 'Get Started',
                 onPressed: () {},
               ),
             ),
-            Text(
-              'Already have account? Log In',
-              style: AppTextStyles.smallText.copyWith(
-                color: AppColors.grey,
-              ),
+            MultiTextButton(
+              onPressed: () => log('tap'),
+              children: [
+                Text(
+                  'Already have account? ',
+                  style: AppTextStyles.smallText.copyWith(
+                    color: AppColors.grey,
+                  ),
+                ),
+                Text(
+                  'Log In ',
+                  style: AppTextStyles.smallText.copyWith(
+                    color: AppColors.greelightTwo,
+                  ),
+                ),
+              ],
             ),
             const SizedBox(height: 24.0),
           ],
