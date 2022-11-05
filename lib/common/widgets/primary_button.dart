f18e403132f88bf17ce035e7912ce0a874efe569
Kaio C. de Oliveira <hello@kaio.dev>
2022-11-05 11:50:06 -0300

    fix: corrige comportamento do botão em listview

--- lib/common/widgets/primary_button.dart
+++ lib/common/widgets/primary_button.dart
@@ -18,26 +18,28 @@ class PrimaryButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Ink(
-      height: 48.0,
-      decoration: BoxDecoration(
-        borderRadius: _borderRadius,
-        gradient: LinearGradient(
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-          colors: onPressed != null
-              ? AppColors.greenGradient
-              : AppColors.greyGradient,
+    return Material(
+      child: Ink(
+        height: 48.0,
+        decoration: BoxDecoration(
+          borderRadius: _borderRadius,
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: onPressed != null
+                ? AppColors.greenGradient
+                : AppColors.greyGradient,
+          ),
         ),
-      ),
-      child: InkWell(
-        borderRadius: _borderRadius,
-        onTap: onPressed,
-        child: Align(
-          child: Text(
-            text,
-            style: AppTextStyles.mediumText18.copyWith(
-              color: AppColors.white,
+        child: InkWell(
+          borderRadius: _borderRadius,
+          onTap: onPressed,
+          child: Align(
+            child: Text(
+              text,
+              style: AppTextStyles.mediumText18.copyWith(
+                color: AppColors.white,
+              ),
             ),
           ),
         ),
