270914f673d8cfc319adf32224f2667a7e0d0bdb
Kaio C. de Oliveira <hello@kaio.dev>
2022-11-05 16:43:15 -0300

    style: adiciona default theme

---
+++ lib/common/themes/default_theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+final defaultTheme = ThemeData(
+  inputDecorationTheme: const InputDecorationTheme(
+    focusedBorder: OutlineInputBorder(
+      borderSide: BorderSide(
+        color: Colors.red,
+      ),
+    ),
+  ),
+);
