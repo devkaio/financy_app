4e72542e1409169840481baa3cbb6811d719936f
Kaio C. de Oliveira <hello@kaio.dev>
2022-11-05 16:44:07 -0300

    style: atualiza text styles

--- lib/common/constants/app_text_styles.dart
+++ lib/common/constants/app_text_styles.dart
@@ -26,4 +26,10 @@ class AppTextStyles {
     fontSize: 14.0,
     fontWeight: FontWeight.w500,
   );
+
+  static const TextStyle inputLabelText = TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 14.0,
+    fontWeight: FontWeight.w400,
+  );
 }
