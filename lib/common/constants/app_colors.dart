62acce5a9f7623e759f4784a0bd4eea82073fa3d
Kaio C. de Oliveira <hello@kaio.dev>
2022-11-07 14:08:55 -0300

    widget: add custom text form field

--- lib/common/constants/app_colors.dart
+++ lib/common/constants/app_colors.dart
@@ -17,4 +17,5 @@ class AppColors {
   static const Color iceWhite = Color(0xFFEEF8F7);
   static const Color darkGrey = Color(0xFF444444);
   static const Color grey = Color(0xFF666666);
+  static const Color error = Color(0xFFF44336);
 }
---
+++ lib/common/widgets/custom_text_form_field.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+import '../constants/app_colors.dart';
+import '../constants/app_text_styles.dart';
+
+class CustomTextFormField extends StatefulWidget {
+  final EdgeInsetsGeometry? padding;
+  final String? hintText;
+  final String? labelText;
+  final TextCapitalization? textCapitalization;
+  final TextEditingController? controller;
+  final TextInputType? keyboardType;
+  final int? maxLength;
+  final TextInputAction? textInputAction;
+
+  const CustomTextFormField({
+    Key? key,
+    this.padding,
+    this.hintText,
+    this.labelText,
+    this.textCapitalization,
+    this.controller,
+    this.keyboardType,
+    this.maxLength,
+    this.textInputAction,
+  }) : super(key: key);
+
+  @override
+  State<CustomTextFormField> createState() => _CustomTextFormFieldState();
+}
+
+class _CustomTextFormFieldState extends State<CustomTextFormField> {
+  final defaultBorder = const OutlineInputBorder(
+    borderSide: BorderSide(
+      color: AppColors.greelightTwo,
+    ),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: widget.padding ??
+          const EdgeInsets.symmetric(
+            horizontal: 24.0,
+            vertical: 12.0,
+          ),
+      child: TextFormField(
+        textInputAction: widget.textInputAction,
+        maxLength: widget.maxLength,
+        keyboardType: widget.keyboardType,
+        controller: widget.controller,
+        textCapitalization:
+            widget.textCapitalization ?? TextCapitalization.none,
+        decoration: InputDecoration(
+          hintText: widget.hintText,
+          floatingLabelBehavior: FloatingLabelBehavior.always,
+          labelText: widget.labelText?.toUpperCase(),
+          labelStyle:
+              AppTextStyles.inputLabelText.copyWith(color: AppColors.grey),
+          focusedBorder: defaultBorder,
+          errorBorder: defaultBorder.copyWith(
+            borderSide: const BorderSide(
+              color: AppColors.error,
+            ),
+          ),
+          focusedErrorBorder: defaultBorder.copyWith(
+            borderSide: const BorderSide(
+              color: AppColors.error,
+            ),
+          ),
+          enabledBorder: defaultBorder,
+          disabledBorder: defaultBorder,
+        ),
+      ),
+    );
+  }
+}
