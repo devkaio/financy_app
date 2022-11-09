a6a7414b80e2b2eb200f71ef1a2b5ddc1459c7e7
Kaio C. de Oliveira <hello@kaio.dev>
2022-11-09 11:42:50 -0300

    style: atualiza nomes de cores

--- lib/common/constants/app_colors.dart
+++ lib/common/constants/app_colors.dart
@@ -3,8 +3,8 @@ import 'dart:ui';
 class AppColors {
   AppColors._();
 
-  static const Color greelightOne = Color(0xFF63B5AF);
-  static const Color greelightTwo = Color(0xFF438883);
+  static const Color greenOne = Color(0xFF438883);
+  static const Color greenTwo = Color(0xFF63B5AF);
   static const List<Color> greenGradient = [
     Color(0xFF63B5AF),
     Color(0xFF438883),
