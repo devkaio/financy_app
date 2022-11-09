4817a8d70a26ee604fdd057f5884dfe30a562a58
Kaio C. de Oliveira <hello@kaio.dev>
2022-11-09 11:43:07 -0300

    style: adiciona estilos inputText e inputHintText

--- lib/common/constants/app_text_styles.dart
+++ lib/common/constants/app_text_styles.dart
@@ -32,4 +32,16 @@ class AppTextStyles {
     fontSize: 14.0,
     fontWeight: FontWeight.w400,
   );
+
+  static const TextStyle inputText = TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 14.0,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const TextStyle inputHintText = TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 12.0,
+    fontWeight: FontWeight.w400,
+  );
 }
