d8d1d8a9648fd0c1218a07238356736c987351bd
Kaio C. de Oliveira <hello@kaio.dev>
2022-11-11 16:23:51 -0300

    feat: adiciona states de sign up

---
+++ lib/features/sign_up/sign_up_state.dart
@@ -0,0 +1,9 @@
+abstract class SignUpState {}
+
+class SignUpInitialState extends SignUpState {}
+
+class SignUpLoadingState extends SignUpState {}
+
+class SignUpSuccessState extends SignUpState {}
+
+class SignUpErrorState extends SignUpState {}
