fe9d86bff76adda262fc2a770479a1772b7636c3
Kaio C. de Oliveira <hello@kaio.dev>
2022-11-12 17:04:20 -0300

    widget: adiciona customModalBottomSheet

---
+++ lib/common/widgets/custom_bottom_sheet.dart
@@ -0,0 +1,53 @@
+import 'package:financy_app/common/constants/app_text_styles.dart';
+import 'package:financy_app/common/widgets/primary_button.dart';
+import 'package:flutter/material.dart';
+
+import '../constants/app_colors.dart';
+
+Future<void> customModalBottomSheet(BuildContext context) {
+  return showModalBottomSheet<void>(
+    context: context,
+    shape: const RoundedRectangleBorder(
+      borderRadius: BorderRadius.only(
+        topLeft: Radius.circular(38.0),
+        topRight: Radius.circular(38.0),
+      ),
+    ),
+    builder: (BuildContext context) {
+      return Container(
+        decoration: const BoxDecoration(
+          color: AppColors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(38.0),
+            topRight: Radius.circular(38.0),
+          ),
+        ),
+        height: 200,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text(
+                'Ops. Algo deu errado.',
+                style: AppTextStyles.mediumText20.copyWith(
+                  color: AppColors.greenOne,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 16.0,
+                  horizontal: 32.0,
+                ),
+                child: PrimaryButton(
+                  text: 'Tentar Novamente',
+                  onPressed: () => Navigator.pop(context),
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    },
+  );
+}
