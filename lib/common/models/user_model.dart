f60baed03e522236634b9d37dc1f843ac36d3478
Kaio C. de Oliveira <hello@kaio.dev>
2022-11-15 13:58:22 -0300

    mode: cria user model

---
+++ lib/common/models/user_model.dart
@@ -0,0 +1,13 @@
+class UserModel {
+  final int? id;
+  final String? name;
+  final String? email;
+  final String? password;
+
+  UserModel({
+    this.id,
+    this.name,
+    this.email,
+    this.password,
+  });
+}
