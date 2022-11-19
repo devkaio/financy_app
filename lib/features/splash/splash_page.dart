43cb8cfe7ae6ac28e52100b64b5cfb8f56076a83
Kaio C. de Oliveira <hello@kaio.dev>
2022-11-19 17:14:52 -0300

    update: adiciona comportament de nevagação para a tela onboarding

--- lib/features/splash/splash_page.dart
+++ lib/features/splash/splash_page.dart
@@ -1,9 +1,38 @@
+import 'dart:async';
+
 import 'package:financy_app/common/constants/app_colors.dart';
 import 'package:financy_app/common/constants/app_text_styles.dart';
+import 'package:financy_app/common/constants/routes.dart';
+import 'package:financy_app/common/widgets/custom_circular_progress_indicator.dart';
 import 'package:flutter/material.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    init();
+  }
+
+  Timer init() {
+    return Timer(
+      const Duration(seconds: 2),
+      navigateToOnboarding,
+    );
+  }
+
+  void navigateToOnboarding() {
+    Navigator.pushReplacementNamed(
+      context,
+      NamedRoute.initial,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,9 +46,15 @@ class SplashPage extends StatelessWidget {
             colors: AppColors.greenGradient,
           ),
         ),
-        child: Text(
-          'financy',
-          style: AppTextStyles.bigText.copyWith(color: AppColors.white),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'financy',
+              style: AppTextStyles.bigText.copyWith(color: AppColors.white),
+            ),
+            const CustomCircularProgressIndicator(),
+          ],
         ),
       ),
     );
