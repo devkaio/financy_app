b2f70c5c866f33a2ca4c03c4256b9ba5614b1dbf
Kaio C. de Oliveira <hello@kaio.dev>
2022-11-20 19:04:57 -0300

    refactor: renomeia arquivo sign in page

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,6 @@
 import 'package:financy_app/common/constants/routes.dart';
 import 'package:financy_app/features/onboarding/onboarding_page.dart';
-import 'package:financy_app/features/sign_in/sing_in_page.dart';
+import 'package:financy_app/features/sign_in/sign_in_page.dart';
 import 'package:financy_app/features/sign_up/sign_up_page.dart';
 import 'package:financy_app/features/splash/splash_page.dart';
 import 'package:flutter/material.dart';
