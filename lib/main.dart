3fa953b9c8ba4c9984dfe327f861d8c7ce9ad1c3
Kaio C. de Oliveira <hello@kaio.dev>
2022-12-10 16:51:36 -0300

    feat: firebase configs

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:financy_app/locator.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'app.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   setupDependencies();
   runApp(const App());
 }
