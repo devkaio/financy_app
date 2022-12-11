fe96765a960a8e3e0e2dc9e1c919fa192f3978a7
Kaio C. de Oliveira <hello@kaio.dev>
2022-12-11 15:44:16 -0300

    feat: salva dados após login

--- lib/features/sign_in/sign_in_controller.dart
+++ lib/features/sign_in/sign_in_controller.dart
@@ -1,4 +1,5 @@
 import 'package:financy_app/services/auth_service.dart';
+import 'package:financy_app/services/secure_storage.dart';
 import 'package:flutter/foundation.dart';
 
 import 'sign_in_state.dart';
@@ -21,15 +22,23 @@ class SignInController extends ChangeNotifier {
     required String email,
     required String password,
   }) async {
+    const secureStorage = SecureStorage();
+
     _changeState(SignInStateLoading());
 
     try {
-      await _service.signIn(
+      final user = await _service.signIn(
         email: email,
         password: password,
       );
 
-      _changeState(SignInStateSuccess());
+      if (user.id != null) {
+        await secureStorage.write(key: "CURRENT_USER", value: user.toJson());
+
+        _changeState(SignInStateSuccess());
+      } else {
+        throw Exception();
+      }
     } catch (e) {
       _changeState(SignInStateError(e.toString()));
     }
