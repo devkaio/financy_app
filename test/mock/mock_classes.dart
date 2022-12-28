19a320b8d2cc0dc091de3f326ad4c540b5c0f7a2
Kaio C. de Oliveira <hello@kaio.dev>
2022-12-28 11:51:25 -0300

    test: add firebase auth sign up test

---
+++ test/mock/mock_classes.dart
@@ -0,0 +1,4 @@
+import 'package:financy_app/services/auth_service.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockFirebaseAuthService extends Mock implements AuthService {}
---
+++ test/services/firebase_auth_service_test.dart
@@ -0,0 +1,68 @@
+import 'package:financy_app/common/models/user_model.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+import '../mock/mock_classes.dart';
+
+void main() {
+  late MockFirebaseAuthService mockFirebaseAuthService;
+  late UserModel user;
+  setUp(() {
+    mockFirebaseAuthService = MockFirebaseAuthService();
+    user = UserModel(
+      name: 'User',
+      email: 'user@email.com',
+      id: '1a2b3c4d5e',
+    );
+  });
+
+  group(
+    'Tests sign up',
+    () {
+      test('Test sign up success', () async {
+        when(
+          () => mockFirebaseAuthService.signUp(
+            name: 'User',
+            email: 'user@email.com',
+            password: 'user@123',
+          ),
+        ).thenAnswer(
+          (_) async => user,
+        );
+
+        final result = await mockFirebaseAuthService.signUp(
+          name: 'User',
+          email: 'user@email.com',
+          password: 'user@123',
+        );
+
+        expect(
+          result,
+          user,
+        );
+      });
+
+      test('Test sign up failure', () async {
+        when(
+          () => mockFirebaseAuthService.signUp(
+            name: 'User',
+            email: 'user@email.com',
+            password: 'user@123',
+          ),
+        ).thenThrow(
+          Exception(),
+        );
+
+        expect(
+          () => mockFirebaseAuthService.signUp(
+            name: 'User',
+            email: 'user@email.com',
+            password: 'user@123',
+          ),
+          // throwsA(isInstanceOf<Exception>()),
+          throwsException,
+        );
+      });
+    },
+  );
+}
