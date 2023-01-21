de6a844b77162e57a3a18ba094df11020afe9a56
Kaio C. de Oliveira <hello@kaio.dev>
2023-01-21 14:59:14 -0300

    chore: add balance card container

--- lib/features/home/home_page.dart
+++ lib/features/home/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
 import '../../common/constants/app_colors.dart';
@@ -94,6 +96,143 @@ class _HomePageState extends State<HomePage> {
                   ),
                 ],
               )),
+          Positioned(
+            left: 24.w,
+            right: 24.w,
+            top: 155.h,
+            child: Container(
+              padding: EdgeInsets.symmetric(
+                horizontal: 24.w,
+                vertical: 32.h,
+              ),
+              decoration: const BoxDecoration(
+                color: AppColors.darkGreen,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(16.0),
+                ),
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Total Balance',
+                            textScaleFactor: textScaleFactor,
+                            style: AppTextStyles.mediumText16w600
+                                .apply(color: AppColors.white),
+                          ),
+                          Text(
+                            '\$ 1,556.00',
+                            textScaleFactor: textScaleFactor,
+                            style: AppTextStyles.mediumText30
+                                .apply(color: AppColors.white),
+                          )
+                        ],
+                      ),
+                      GestureDetector(
+                        onTap: () => log('options'),
+                        child: PopupMenuButton(
+                            padding: EdgeInsets.zero,
+                            child: const Icon(
+                              Icons.more_horiz,
+                              color: AppColors.white,
+                            ),
+                            itemBuilder: (context) => [
+                                  const PopupMenuItem(
+                                    height: 24.0,
+                                    child: Text("Item 1"),
+                                  ),
+                                ]),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 36.h),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Container(
+                            padding: const EdgeInsets.all(4.0),
+                            decoration: BoxDecoration(
+                              color: AppColors.white.withOpacity(0.06),
+                              borderRadius: const BorderRadius.all(
+                                Radius.circular(16.0),
+                              ),
+                            ),
+                            child: Icon(
+                              Icons.arrow_downward,
+                              color: AppColors.white,
+                              size: iconSize,
+                            ),
+                          ),
+                          const SizedBox(width: 4.0),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Income',
+                                textScaleFactor: textScaleFactor,
+                                style: AppTextStyles.mediumText16w500
+                                    .apply(color: AppColors.white),
+                              ),
+                              Text(
+                                '\$ 1,840.00',
+                                textScaleFactor: textScaleFactor,
+                                style: AppTextStyles.mediumText20
+                                    .apply(color: AppColors.white),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Container(
+                            padding: const EdgeInsets.all(4.0),
+                            decoration: BoxDecoration(
+                              color: AppColors.white.withOpacity(0.06),
+                              borderRadius: const BorderRadius.all(
+                                Radius.circular(16.0),
+                              ),
+                            ),
+                            child: Icon(
+                              Icons.arrow_upward,
+                              color: AppColors.white,
+                              size: iconSize,
+                            ),
+                          ),
+                          const SizedBox(width: 4.0),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Expenses',
+                                textScaleFactor: textScaleFactor,
+                                style: AppTextStyles.mediumText16w500
+                                    .apply(color: AppColors.white),
+                              ),
+                              Text(
+                                '\$ 2,824.00',
+                                textScaleFactor: textScaleFactor,
+                                style: AppTextStyles.mediumText20
+                                    .apply(color: AppColors.white),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
