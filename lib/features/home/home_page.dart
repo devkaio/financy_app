7f2854bbfdca76be39a604c3d2c35866ca796916
Kaio C. de Oliveira <hello@kaio.dev>
2023-01-21 14:58:32 -0300

    chore: add user greeting and notification icon

--- lib/features/home/home_page.dart
+++ lib/features/home/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../../common/constants/app_colors.dart';
+import '../../common/constants/app_text_styles.dart';
 import '../../common/extensions/sizes.dart';
 
 class HomePage extends StatefulWidget {
@@ -37,6 +38,62 @@ class _HomePageState extends State<HomePage> {
               height: 287.h,
             ),
           ),
+          Positioned(
+              left: 24.0,
+              right: 24.0,
+              top: 74.h,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Good afternoon,',
+                        textScaleFactor: textScaleFactor,
+                        style: AppTextStyles.smallText
+                            .apply(color: AppColors.white),
+                      ),
+                      Text(
+                        'Enjelin Morgeana',
+                        textScaleFactor: textScaleFactor,
+                        style: AppTextStyles.mediumText20
+                            .apply(color: AppColors.white),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(
+                      vertical: 8.h,
+                      horizontal: 8.w,
+                    ),
+                    decoration: BoxDecoration(
+                      borderRadius:
+                          const BorderRadius.all(Radius.circular(4.0)),
+                      color: AppColors.white.withOpacity(0.06),
+                    ),
+                    child: Stack(
+                      alignment: const AlignmentDirectional(0.5, -0.5),
+                      children: [
+                        const Icon(
+                          Icons.notifications_none_outlined,
+                          color: AppColors.white,
+                        ),
+                        Container(
+                          width: 8.w,
+                          height: 8.w,
+                          decoration: BoxDecoration(
+                            color: AppColors.notification,
+                            borderRadius: BorderRadius.circular(
+                              4.0,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              )),
         ],
       ),
     );
