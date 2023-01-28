d0ff33e8090a134898292713888af159e49a1d23
Kaio C. de Oliveira <hello@kaio.dev>
2023-01-28 16:45:34 -0300

    style: update custom circular progress indicator

--- lib/common/widgets/custom_circular_progress_indicator.dart
+++ lib/common/widgets/custom_circular_progress_indicator.dart
@@ -1,17 +1,20 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 
 import '../constants/app_colors.dart';
 
 class CustomCircularProgressIndicator extends StatelessWidget {
+  final Color? color;
   const CustomCircularProgressIndicator({
     Key? key,
+    this.color,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
+    return Center(
       child: CircularProgressIndicator(
-        color: AppColors.iceWhite,
+        color: color ?? AppColors.iceWhite,
       ),
     );
   }
