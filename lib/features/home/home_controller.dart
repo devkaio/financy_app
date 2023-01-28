f179c3b76c61fc636779ac2b9486d0fdbe80fd41
Kaio C. de Oliveira <hello@kaio.dev>
2023-01-28 16:46:04 -0300

    chore: add home controller

---
+++ lib/features/home/home_controller.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/foundation.dart';
+
+import '../../common/models/transaction_model.dart';
+import '../../repositories/transaction_repository.dart';
+import 'home_state.dart';
+
+class HomeController extends ChangeNotifier {
+  final TransactionRepository _transactionRepository;
+  HomeController(this._transactionRepository);
+
+  HomeState _state = HomeStateInitial();
+
+  HomeState get state => _state;
+
+  List<TransactionModel> _transactions = [];
+  List<TransactionModel> get transactions => _transactions;
+
+  void _changeState(HomeState newState) {
+    _state = newState;
+    notifyListeners();
+  }
+
+  Future<void> getAllTransactions() async {
+    _changeState(HomeStateLoading());
+    try {
+      _transactions = await _transactionRepository.getAllTransactions();
+      _changeState(HomeStateSuccess());
+    } catch (e) {
+      _changeState(HomeStateError());
+    }
+  }
+}
