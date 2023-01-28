25b9870ea83f9cc7b8df039bf1e1c7804f2a5c6e
Kaio C. de Oliveira <hello@kaio.dev>
2023-01-28 16:45:04 -0300

    chore: add transaction model

---
+++ lib/common/models/transaction_model.dart
@@ -0,0 +1,10 @@
+class TransactionModel {
+  final String title;
+  final double value;
+  final int date;
+  TransactionModel({
+    required this.title,
+    required this.value,
+    required this.date,
+  });
+}
