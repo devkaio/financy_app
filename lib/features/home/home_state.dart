c7f33ffb88ab3c90e0f48499da28dc34a6f1b830
Kaio C. de Oliveira <hello@kaio.dev>
2023-01-28 16:45:49 -0300

    chore: add home state

---
+++ lib/features/home/home_state.dart
@@ -0,0 +1,9 @@
+abstract class HomeState {}
+
+class HomeStateInitial extends HomeState {}
+
+class HomeStateLoading extends HomeState {}
+
+class HomeStateSuccess extends HomeState {}
+
+class HomeStateError extends HomeState {}
