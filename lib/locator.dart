90d530af135ea4ae9a19510ccb8f02cf0da5e06a
Kaio C. de Oliveira <hello@kaio.dev>
2023-01-28 16:46:23 -0300

    chore: update service locator

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,8 +1,10 @@
 import 'package:get_it/get_it.dart';
 
+import 'features/home/home_controller.dart';
 import 'features/sign_in/sign_in_controller.dart';
 import 'features/sign_up/sign_up_controller.dart';
 import 'features/splash/splash_controller.dart';
+import 'repositories/transaction_repository.dart';
 import 'services/auth_service.dart';
 import 'services/firebase_auth_service.dart';
 import 'services/secure_storage.dart';
@@ -31,4 +33,10 @@ void setupDependencies() {
       const SecureStorage(),
     ),
   );
+
+  locator.registerFactory<TransactionRepository>(
+      () => TransactionRepositoryImpl());
+
+  locator.registerLazySingleton<HomeController>(
+      () => HomeController(locator.get<TransactionRepository>()));
 }
