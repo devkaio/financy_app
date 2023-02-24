1f8bdb0e0e8735efdf28596d9671978b46ceb110
Kaio C. de Oliveira <hello@kaio.dev>
2023-02-24 12:26:17 -0300

    widget: create transaction listview

---
+++ lib/features/home/widgets/transaction_listview.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+import '../../../common/constants/app_colors.dart';
+import '../../../common/constants/app_text_styles.dart';
+import '../../../common/models/transaction_model.dart';
+
+class TransactionListView extends StatelessWidget {
+  const TransactionListView({
+    super.key,
+    required this.transactionList,
+    this.itemCount,
+  });
+
+  final List<TransactionModel> transactionList;
+  final int? itemCount;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      physics: const BouncingScrollPhysics(),
+      padding: EdgeInsets.zero,
+      itemCount: itemCount ?? transactionList.length,
+      itemBuilder: (context, index) {
+        final item = transactionList[index];
+
+        final color =
+            item.value.isNegative ? AppColors.outcome : AppColors.income;
+        final value = "\$${item.value.toStringAsFixed(2)}";
+        return ListTile(
+          contentPadding: const EdgeInsets.symmetric(horizontal: 8.0),
+          leading: Container(
+            decoration: const BoxDecoration(
+              color: AppColors.antiFlashWhite,
+              borderRadius: BorderRadius.all(Radius.circular(8.0)),
+            ),
+            padding: const EdgeInsets.all(8.0),
+            child: const Icon(
+              Icons.monetization_on_outlined,
+            ),
+          ),
+          title: Text(
+            item.description,
+            style: AppTextStyles.mediumText16w500,
+          ),
+          subtitle: Text(
+            DateTime.fromMillisecondsSinceEpoch(item.date).toString(),
+            style: AppTextStyles.smallText13,
+          ),
+          trailing: Text(
+            value,
+            style: AppTextStyles.mediumText18.apply(color: color),
+          ),
+        );
+      },
+    );
+  }
+}
