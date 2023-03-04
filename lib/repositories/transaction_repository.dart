ef56535a0eca6668d44aa852a86079a758f4307e
Kaio C. de Oliveira <hello@kaio.dev>
2023-03-04 15:40:02 -0300

    chore: add get balance repository method

--- lib/repositories/transaction_repository.dart
+++ lib/repositories/transaction_repository.dart
@@ -1,6 +1,8 @@
 import 'package:graphql_flutter/graphql_flutter.dart';
 
 import '../common/constants/queries/get_all_transactions.dart';
+import '../common/constants/queries/get_balances.dart';
+import '../common/models/balances_model.dart';
 import '../common/models/transaction_model.dart';
 import '../locator.dart';
 import '../services/graphql_service.dart';
@@ -8,6 +10,8 @@ import '../services/graphql_service.dart';
 abstract class TransactionRepository {
   Future<void> addTransaction();
   Future<List<TransactionModel>> getAllTransactions();
+
+  Future<BalancesModel> getBalances();
 }
 
 class TransactionRepositoryImpl implements TransactionRepository {
@@ -22,14 +26,25 @@ class TransactionRepositoryImpl implements TransactionRepository {
   @override
   Future<List<TransactionModel>> getAllTransactions() async {
     try {
-      final response =
-          await client.query(QueryOptions(document: gql(qGetAllTransactions)));
+      final response = await client.query(QueryOptions(document: gql(qGetAllTransactions)));
 
       final parsedData = List.from(response.data?['transaction'] ?? []);
 
-      final transactions =
-          parsedData.map((e) => TransactionModel.fromMap(e)).toList();
+      final transactions = parsedData.map((e) => TransactionModel.fromMap(e)).toList();
       return transactions;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  @override
+  Future<BalancesModel> getBalances() async {
+    try {
+      final response = await client.query(QueryOptions(document: gql(qGetBalances)));
+
+      final balances = BalancesModel.fromMap(response.data ?? {});
+
+      return balances;
     } catch (e) {
       rethrow;
     }
