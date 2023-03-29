7b1c75b4ad9e62eb5a5264047f3b8a03e93ebb20
Kaio C. de Oliveira <hello@kaio.dev>
2023-03-29 09:15:06 -0300

    style: update default theme

--- lib/common/themes/default_theme.dart
+++ lib/common/themes/default_theme.dart
@@ -1,11 +1,58 @@
 import 'package:flutter/material.dart';
 
-final defaultTheme = ThemeData(
-  inputDecorationTheme: const InputDecorationTheme(
-    focusedBorder: OutlineInputBorder(
+import '../constants/app_colors.dart';
+import '../constants/app_text_styles.dart';
+
+class CustomTheme {
+  CustomTheme._();
+
+  factory CustomTheme() {
+    return CustomTheme._();
+  }
+
+  ThemeData get defaultTheme {
+    const defaultBorder = OutlineInputBorder(
       borderSide: BorderSide(
-        color: Colors.red,
+        color: AppColors.greenOne,
       ),
-    ),
-  ),
-);
+    );
+
+    return ThemeData(
+      colorScheme: const ColorScheme.light(
+        primary: AppColors.darkGreen,
+      ),
+      floatingActionButtonTheme: const FloatingActionButtonThemeData(
+        foregroundColor: AppColors.iceWhite,
+        backgroundColor: AppColors.green,
+      ),
+      textButtonTheme: TextButtonThemeData(
+        style: TextButton.styleFrom(
+          foregroundColor: AppColors.darkGreen,
+        ),
+      ),
+      tabBarTheme: const TabBarTheme(
+        indicator: BoxDecoration(
+          border: Border(),
+        ),
+      ),
+      inputDecorationTheme: InputDecorationTheme(
+        labelStyle:
+            AppTextStyles.inputLabelText.copyWith(color: AppColors.grey),
+        hintStyle: AppTextStyles.inputHintText.copyWith(color: AppColors.green),
+        focusedBorder: defaultBorder,
+        enabledBorder: defaultBorder,
+        disabledBorder: defaultBorder,
+        errorBorder: defaultBorder.copyWith(
+          borderSide: const BorderSide(
+            color: AppColors.error,
+          ),
+        ),
+        focusedErrorBorder: defaultBorder.copyWith(
+          borderSide: const BorderSide(
+            color: AppColors.error,
+          ),
+        ),
+      ),
+    );
+  }
+}
