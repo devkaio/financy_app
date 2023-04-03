ee9d60e440d0cb0069c8a54bf0770388feb43c95
Kaio C. de Oliveira <hello@kaio.dev>
2023-04-03 10:11:41 -0300

    update custom bottom app bar

--- lib/common/widgets/custom_bottom_app_bar.dart
+++ lib/common/widgets/custom_bottom_app_bar.dart
@@ -1,14 +1,19 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
 import '../constants/app_colors.dart';
+import '../extensions/page_controller_ext.dart';
 
 class CustomBottomAppBar extends StatefulWidget {
+  final PageController controller;
   final Color? selectedItemColor;
   final List<CustomBottomAppBarItem> children;
   const CustomBottomAppBar({
     Key? key,
     this.selectedItemColor,
     required this.children,
+    required this.controller,
   })  : assert(children.length == 5, 'children.length must be 5'),
         super(key: key);
 
@@ -17,7 +22,23 @@ class CustomBottomAppBar extends StatefulWidget {
 }
 
 class _CustomBottomAppBarState extends State<CustomBottomAppBar> {
-  int _selectedItemIndex = 0;
+  @override
+  void initState() {
+    widget.controller.addListener(() {
+      setState(() {
+        log(
+          widget.controller.selectedBottomAppBarItemIndex.toString(),
+        );
+      });
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    widget.controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,24 +48,31 @@ class _CustomBottomAppBarState extends State<CustomBottomAppBar> {
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: widget.children.map(
           (item) {
-            final currentItem =
-                widget.children.indexOf(item) == _selectedItemIndex;
-            return Expanded(
-              child: InkWell(
-                onTap: item.onPressed,
-                onTapUp: (_) => setState(() {
-                  _selectedItemIndex = widget.children.indexOf(item);
-                }),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 12.0),
-                  child: Icon(
-                    currentItem ? item.primaryIcon : item.secondaryIcon,
-                    color: currentItem
-                        ? widget.selectedItemColor
-                        : AppColors.lightGrey,
+            bool currentItem;
+
+            currentItem = widget.children.indexOf(item) ==
+                widget.controller.selectedBottomAppBarItemIndex;
+            return Builder(
+              builder: (context) {
+                return Expanded(
+                  child: InkWell(
+                    onTap: item.onPressed,
+                    onTapUp: (_) {
+                      widget.controller.setBottomAppBarItemIndex =
+                          widget.children.indexOf(item);
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 12.0),
+                      child: Icon(
+                        currentItem ? item.primaryIcon : item.secondaryIcon,
+                        color: currentItem
+                            ? widget.selectedItemColor
+                            : AppColors.lightGrey,
+                      ),
+                    ),
                   ),
-                ),
-              ),
+                );
+              },
             );
           },
         ).toList(),
