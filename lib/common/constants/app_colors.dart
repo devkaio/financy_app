1554366c7d854f4d2cc0238690821ec4eda45319
Kaio C. de Oliveira <hello@kaio.dev>
2023-04-14 10:06:28 -0300

    style: update appcolors

--- lib/common/constants/app_colors.dart
+++ lib/common/constants/app_colors.dart
@@ -16,6 +16,7 @@ class AppColors {
   static const Color white = Color(0xFFFFFFFF);
   static const Color iceWhite = Color(0xFFEEF8F7);
   static const Color antiFlashWhite = Color(0xFFF0F6F5);
+  static const Color blackGrey = Color(0xFF222222);
   static const Color darkGrey = Color(0xFF444444);
   static const Color grey = Color(0xFF666666);
   static const Color lightGrey = Color(0xFFAAAAAA);
