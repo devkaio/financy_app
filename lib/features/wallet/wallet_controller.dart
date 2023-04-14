b582b1f4ce814750bafe6f852020a2eaa369894a
Kaio C. de Oliveira <hello@kaio.dev>
2023-04-14 10:11:48 -0300

    feat: add wallet controller

---
+++ lib/features/wallet/wallet_controller.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/foundation.dart';
+
+import '../../common/models/transaction_model.dart';
+import '../../repositories/transaction_repository.dart';
+import 'wallet_state.dart';
+
+class WalletController extends ChangeNotifier {
+  final TransactionRepository repository;
+
+  WalletController({
+    required this.repository,
+  });
+
+  WalletState _state = WalletStateInitial();
+
+  WalletState get state => _state;
+
+  List<TransactionModel> _transactions = [];
+  List<TransactionModel> get transactions => _transactions;
+
+  int get _limit => 10;
+  int get _offset => transactions.isEmpty ? 0 : transactions.length;
+
+  bool _isLoading = false;
+
+  bool get isLoading {
+    return _isLoading;
+  }
+
+  void _changeState(WalletState newState) {
+    _state = newState;
+    notifyListeners();
+  }
+
+  Future<void> getAllTransactions() async {
+    _changeState(WalletStateLoading());
+
+    try {
+      if (transactions.isNotEmpty) transactions.clear();
+
+      _transactions = await repository.getAllTransactions(
+        limit: _limit,
+        offset: _offset,
+      );
+      if (_offset >= _limit) {
+        _isLoading = true;
+      }
+      _changeState(WalletStateSuccess());
+    } catch (e) {
+      _changeState(WalletStateError());
+    }
+  }
+
+  void get fetchMore async {
+    try {
+      if (isLoading) {
+        final result = await repository.getAllTransactions(
+          limit: _limit,
+          offset: _offset,
+        );
+        if (result.isNotEmpty) {
+          _transactions.addAll(result);
+        } else {
+          _isLoading = false;
+        }
+      }
+      _changeState(WalletStateSuccess());
+    } catch (e) {
+      _changeState(WalletStateError());
+    }
+  }
+}
