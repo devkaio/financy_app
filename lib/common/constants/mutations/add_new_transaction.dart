32e854d8b2831ffc06ae715edc3e36ed8db1f1dc
Kaio C. de Oliveira <hello@kaio.dev>
2023-05-12 12:15:13 -0300

    const: update add new transaction mutation with uuid

--- lib/common/constants/mutations/add_new_transaction.dart
+++ lib/common/constants/mutations/add_new_transaction.dart
@@ -1,6 +1,6 @@
 const String mAddNewTransaction = r"""
-mutation addNewTransaction($category: String!, $date: timestamptz!, $description: String!, $status: Boolean!, $value: numeric!, $user_id: String!) {
-  insert_transaction_one(object: {date: $date, description: $description, status: $status, value: $value, category: $category, user_id: $user_id}) {
+mutation addNewTransaction($id: uuid!, $category: String!, $date: timestamptz!, $created_at: timestamptz!, $description: String!, $status: Boolean!, $value: numeric!, $user_id: String!) {
+  insert_transaction_one(object: {id: $id, date: $date, created_at: $created_at, description: $description, status: $status, value: $value, category: $category, user_id: $user_id}) {
     category
     created_at
     date
