1064098b39c832f9dcff4592bedf237ccb746ed6
Kaio C. de Oliveira <hello@kaio.dev>
2023-05-12 12:16:41 -0300

    util: update date formatter to comply with hasura's timestamp

--- lib/common/extensions/date_formatter.dart
+++ lib/common/extensions/date_formatter.dart
@@ -12,4 +12,13 @@ extension DateTimeFormatter on DateTime {
 
     return DateFormat('EEE, MMM d, ' 'yy').format(this);
   }
+
+  String get formatISOTime {
+    var duration = timeZoneOffset;
+    if (duration.isNegative) {
+      return ("${toIso8601String().replaceAll('Z', '-')}${duration.inHours.toString().padLeft(2, '0')}:${(duration.inMinutes - (duration.inHours * 60)).toString().padLeft(2, '0')}");
+    } else {
+      return ("${toIso8601String().replaceAll('Z', '+')}${duration.inHours.toString().padLeft(2, '0')}:${(duration.inMinutes - (duration.inHours * 60)).toString().padLeft(2, '0')}");
+    }
+  }
 }
