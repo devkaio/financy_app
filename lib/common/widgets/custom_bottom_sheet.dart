0c795b678cd176e67ab14d2e7f3e050c2abd027b
Kaio C. de Oliveira <hello@kaio.dev>
2023-05-17 08:59:03 -0300

    add dismissible property in custom modal sheet

--- lib/common/widgets/custom_bottom_sheet.dart
+++ lib/common/widgets/custom_bottom_sheet.dart
@@ -11,10 +11,12 @@ mixin CustomModalSheetMixin<T extends StatefulWidget> on State<T> {
     String? buttonText,
     VoidCallback? onPressed,
     List<Widget>? actions,
+    bool isDismissible = true,
   }) {
     assert(buttonText != null || actions != null);
 
     return showModalBottomSheet(
+      isDismissible: isDismissible,
       context: context,
       shape: const RoundedRectangleBorder(
         borderRadius: BorderRadius.only(
@@ -23,47 +25,50 @@ mixin CustomModalSheetMixin<T extends StatefulWidget> on State<T> {
         ),
       ),
       builder: (BuildContext context) {
-        return Container(
-          decoration: const BoxDecoration(
-            color: AppColors.white,
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(38.0),
-              topRight: Radius.circular(38.0),
+        return WillPopScope(
+          onWillPop: () => Future.value(isDismissible),
+          child: Container(
+            decoration: const BoxDecoration(
+              color: AppColors.white,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(38.0),
+                topRight: Radius.circular(38.0),
+              ),
             ),
-          ),
-          height: 200,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                  vertical: 16.0,
-                  horizontal: 32.0,
-                ),
-                child: Text(
-                  content,
-                  style: AppTextStyles.mediumText20.copyWith(
-                    color: AppColors.greenOne,
+            height: 200,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 16.0,
+                    horizontal: 32.0,
+                  ),
+                  child: Text(
+                    content,
+                    style: AppTextStyles.mediumText20.copyWith(
+                      color: AppColors.greenOne,
+                    ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                  vertical: 8.0,
-                  horizontal: 32.0,
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 8.0,
+                    horizontal: 32.0,
+                  ),
+                  child: actions != null
+                      ? Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: actions,
+                        )
+                      : PrimaryButton(
+                          text: buttonText!,
+                          onPressed: onPressed ?? () => Navigator.pop(context),
+                        ),
                 ),
-                child: actions != null
-                    ? Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: actions,
-                      )
-                    : PrimaryButton(
-                        text: buttonText!,
-                        onPressed: onPressed ?? () => Navigator.pop(context),
-                      ),
-              ),
-            ],
+              ],
+            ),
           ),
         );
       },
