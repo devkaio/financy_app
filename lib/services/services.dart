330e541ab2be31609c8344bc9c178fbb9aa53a93
Kaio C. de Oliveira <hello@kaio.dev>
2023-06-17 15:36:47 -0300

    refactor: add services barrel file

---
+++ lib/services/services.dart
@@ -0,0 +1,7 @@
+export 'auth_service.dart';
+export 'connection_service.dart';
+export 'data_service.dart';
+export 'database_service.dart';
+export 'firebase_auth_service.dart';
+export 'graphql_service.dart';
+export 'secure_storage.dart';
