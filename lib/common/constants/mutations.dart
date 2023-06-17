5927bf5721271264e3f7424e5c291babcbbb2fa5
Kaio C. de Oliveira <hello@kaio.dev>
2023-06-17 15:22:38 -0300

    refactor: removes duplicated params from updateTransaction mutation

--- lib/common/constants/mutations.dart
+++ lib/common/constants/mutations.dart
@@ -14,14 +14,16 @@ mutation addNewTransaction($id: uuid!, $category: String!, $date: timestamptz!, 
 }""";
 
   static const String mUpdateTransaction = r"""
-mutation updateTransaction($id: uuid!, $category: String!, $date: timestamptz!, $description: String, $status: Boolean!, $value: numeric!, $id1: uuid = "", $category1: String = "", $date1: timestamptz = "", $description1: String = "", $status1: Boolean = false, $value1: numeric = "") {
-  update_transaction_by_pk(pk_columns: {id: $id}, _set: {category: $category, date: $date, description: $description, status: $status, value: $value}) {
+mutation updateTransaction($id: uuid!, $category: String!, $date: timestamptz!, $description: String!, $value: numeric!, $status: Boolean!, $created_at: timestamptz!) {
+  update_transaction_by_pk(pk_columns: {id: $id}, _set: { category: $category, date: $date, description: $description, status: $status, value: $value, created_at: $created_at}) {
     category
+    created_at
     date
     description
     id
     status
     value
+    user_id
   }
 }
 """;
