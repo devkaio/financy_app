8355600a3b077214a4edfb94122d1df5a7af1375
Kaio C. de Oliveira <hello@kaio.dev>
2023-06-23 14:36:55 -0300

    test: update controllers unit tests

--- test/mock/mock_classes.dart
+++ test/mock/mock_classes.dart
@@ -1,12 +1,13 @@
 import 'package:financy_app/common/models/user_model.dart';
+import 'package:financy_app/repositories/repositories.dart';
 import 'package:financy_app/services/services.dart';
 import 'package:mocktail/mocktail.dart';
 
-//Mock Models
+// Mock Models
 
 class MockUser extends Mock implements UserModel {}
 
-//Mock Services
+// Mock Services
 class MockFirebaseAuthService extends Mock implements AuthService {}
 
 class MockSecureStorageService extends Mock implements SecureStorageService {}
@@ -16,3 +17,7 @@ class MockDatabaseService extends Mock implements DatabaseService {}
 class MockGraphQLService extends Mock implements GraphQLService {}
 
 class MockSyncService extends Mock implements SyncService {}
+
+// Mock Repositories
+
+class MockTransactionRepository extends Mock implements TransactionRepository {}
