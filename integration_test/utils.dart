ea9bd8e16efa46fb5cc877a338dd1065c3a8607d
Kaio C. de Oliveira <hello@kaio.dev>
2023-06-24 17:30:04 -0300

    test: add utils

---
+++ integration_test/utils.dart
@@ -0,0 +1,39 @@
+import 'package:financy_app/app.dart';
+import 'package:financy_app/firebase_options.dart';
+import 'package:financy_app/locator.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+class Utils {
+  const Utils();
+
+  /// Setup dependencies and returns [App] widget.
+  /// Usually called in [setUp] method within main test.
+  Future<Widget> createAppUnderTest() async {
+    await locator.reset();
+
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+
+    setupDependencies();
+
+    await locator.allReady();
+
+    return const App();
+  }
+
+  Future<void> dragUntilVisible(
+    WidgetTester tester, {
+    required Finder target,
+    required Finder scrollable,
+  }) async {
+    await tester.dragUntilVisible(
+      target,
+      scrollable,
+      const Offset(0, -50),
+    );
+    await tester.pumpAndSettle();
+  }
+}
