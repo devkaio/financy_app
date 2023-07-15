1f9b1eeaca98eb3c1882fd6e9ccb56b26c26c36e
Kaio C. de Oliveira <hello@kaio.dev>
2023-07-15 11:35:25 -0300

    update profile page

--- lib/features/profile/profile_page.dart
+++ lib/features/profile/profile_page.dart
@@ -1,3 +1,4 @@
+import 'package:financy_app/common/extensions/extensions.dart';
 import 'package:flutter/material.dart';
 
 import '../../common/constants/constants.dart';
@@ -55,41 +56,148 @@ class _ProfilePageState extends State<ProfilePage> with CustomModalSheetMixin {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text("Profile"),
-            AnimatedBuilder(
-              animation: _profileController,
-              builder: (context, child) {
-                if (_profileController.state is ProfileStateSuccess) {
-                  final user =
-                      (_profileController.state as ProfileStateSuccess).user;
-                  return Column(
-                    children: [
-                      Text(user.name!),
-                      Text(user.email!),
-                    ],
-                  );
-                }
-                return const SizedBox.shrink();
-              },
+      body: Stack(
+        children: [
+          const AppHeader(
+            title: 'Profile',
+          ),
+          Positioned(
+            top: 210.h,
+            left: 0,
+            right: 0,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  minRadius: 60.h,
+                  child: const Icon(Icons.person),
+                ),
+                SizedBox(height: 20.h),
+                AnimatedBuilder(
+                  animation: _profileController,
+                  builder: (context, child) {
+                    if (_profileController.state is ProfileStateSuccess) {
+                      final user =
+                          (_profileController.state as ProfileStateSuccess)
+                              .user;
+                      return Column(
+                        children: [
+                          Text(
+                            user.name!,
+                            style: AppTextStyles.mediumText20,
+                          ),
+                          SizedBox(height: 4.h),
+                          Text(
+                            user.email!,
+                            style: AppTextStyles.smallText.apply(
+                              color: AppColors.green,
+                            ),
+                          ),
+                        ],
+                      );
+                    }
+                    return const SizedBox.shrink();
+                  },
+                ),
+              ],
             ),
-            TextButton(
-              key: Keys.profilePagelogoutButton,
-              onPressed: () async {
-                await locator.get<AuthService>().signOut();
-                await const SecureStorageService().deleteAll();
-                await locator.get<DatabaseService>().deleteDB;
-                if (mounted) {
-                  Navigator.popUntil(context, ModalRoute.withName('/'));
-                }
-              },
-              child: const Text("Logout"),
-            )
-          ],
-        ),
+          ),
+          Positioned(
+            top: 450.h,
+            left: 32,
+            right: 32,
+            bottom: 0,
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  InkWell(
+                    onTap: () {},
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          const Icon(
+                            Icons.person,
+                            color: AppColors.green,
+                          ),
+                          const SizedBox(width: 8.0),
+                          Flexible(
+                            child: Text(
+                              'Change name',
+                              style: AppTextStyles.mediumText16w500
+                                  .apply(color: AppColors.green),
+                              textAlign: TextAlign.start,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 8.0),
+                  InkWell(
+                    onTap: () {},
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          const Icon(
+                            Icons.lock_person_rounded,
+                            color: AppColors.green,
+                          ),
+                          const SizedBox(width: 8.0),
+                          Flexible(
+                            child: Text(
+                              'Change password',
+                              style: AppTextStyles.mediumText16w500
+                                  .apply(color: AppColors.green),
+                              textAlign: TextAlign.start,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 8.0),
+                  InkWell(
+                    onTap: () async {
+                      await locator.get<AuthService>().signOut();
+                      await locator.get<SecureStorageService>().deleteAll();
+                      await locator.get<DatabaseService>().deleteDB;
+                      if (!mounted) return;
+
+                      Navigator.popUntil(
+                        context,
+                        ModalRoute.withName(NamedRoute.initial),
+                      );
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          const Icon(
+                            Icons.logout_outlined,
+                            color: AppColors.green,
+                          ),
+                          const SizedBox(width: 8.0),
+                          Flexible(
+                            child: Text(
+                              'Logout',
+                              style: AppTextStyles.mediumText16w500
+                                  .apply(color: AppColors.green),
+                              textAlign: TextAlign.start,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
