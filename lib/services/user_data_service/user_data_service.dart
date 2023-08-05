bd6a85e7fa2030d6fd65c8223cd58fca41790370
Kaio C. de Oliveira <hello@kaio.dev>
2023-08-05 13:13:35 -0300

    update user data service interface

--- lib/services/user_data_service/user_data_service.dart
+++ lib/services/user_data_service/user_data_service.dart
@@ -3,4 +3,10 @@ import 'package:financy_app/common/models/models.dart';
 
 abstract class UserDataService {
   Future<DataResult<UserModel>> getUserData();
+
+  Future<DataResult<UserModel>> updateUserName(String newUserName);
+
+  Future<DataResult<bool>> updatePassword(String newPassword);
+
+  UserModel get userData;
 }
