8bb539e070bfea46f9122cf0e153fc561d1dc642
Kaio C. de Oliveira <hello@kaio.dev>
2023-08-19 14:16:03 -0300

    add stats feature

---
+++ lib/features/stats/stats_state.dart
@@ -0,0 +1,9 @@
+abstract class StatsState {}
+
+class StatsStateInitial extends StatsState {}
+
+class StatsStateLoading extends StatsState {}
+
+class StatsStateSuccess extends StatsState {}
+
+class StatsStateError extends StatsState {}
