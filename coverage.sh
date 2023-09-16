0fb64113eeababe1201657dc182e33122178ef4f
Kaio C. de Oliveira <hello@kaio.dev>
2023-09-16 11:11:11 -0300

    update coverage script

--- coverage.sh
+++ coverage.sh
@@ -2,8 +2,18 @@
 # Create coverage import file
 file=test/coverage_helper_test.dart
 
-# Import dart files
-echo "// Helper file to make coverage work for all dart files\n" > $file
-echo "// ignore_for_file: unused_import, avoid_relative_lib_imports" >> $file
-find lib '!' -path '*generated*/*' '!' -name '*.g.dart' '!' -name '*.part.dart' '!' -name '*.freezed.dart' -name '*.dart' | awk -v package=$(grep -oP '(?<=name: ).*' pubspec.yaml | tr -d '\n') '{ relative_path = $0; gsub(/^lib\//, "", relative_path); printf "import '\''../lib/%s'\'';\n", relative_path }' >> $file
-echo -e "\nvoid main() {}" >> $file
+# Grant write permissions to the test/ directory
+chmod +w test/
+
+# Import dart files with "package:financy_app/" prefix
+echo "// Helper file to make coverage work for all dart files\n" > "$file"
+echo "// ignore_for_file: unused_import, avoid_relative_lib_imports" >> "$file"
+
+# Find Dart files and generate import statements
+find lib -type f -name "*.dart" ! -path "lib/generated/*" | while read -r dart_file; do
+  relative_path="${dart_file#lib/}"
+  echo "import 'package:financy_app/$relative_path';" >> "$file"
+done
+
+echo -e "\nvoid main() {}" >> "$file"
+
